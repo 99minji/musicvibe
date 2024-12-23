ed3f7d78dea257b29d576e6a2ccb2d3a91845f39
99uminji <rnalswl96@naver.com>
2024-12-23 23:15:10 +0900

    Chore : Next 설치

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <button>로그인</button>
+    </div>
+  );
+};
+
+export default Home;
